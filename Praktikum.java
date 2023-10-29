7f3350d605091ef655700cef19ec7682aceb0ba5
EugenSnegirjow <EugenSnegirjow@yandex.ru>
2023-10-30 00:51:12 +0300

    Написал комментарии как улучшить код

--- Praktikum.java
+++ Praktikum.java
@@ -8,20 +8,31 @@ public class Praktikum {
         int year = scanner.nextInt();
 
         if (isLeapYear(year)) {         // вывод решения по значению true/false
-            // System.out.println("Укзаанный год " + year + " будет високосный");
-            System.out.println("вывод: 12.09." + year);
+            // System.out.println("Указанный год " + year + " будет високосный");
+            System.out.println("вывод: 12.09." + year); /** По условию задачи нужно делать вывод в формате "12.09.2000",
+             слово "вывод" выводить не нужно*/
 
         } else {
-           //  System.out.println("Укзаанный год " + year + " будет не високосный");
+           //  System.out.println("Указанный год " + year + " будет не високосный");
             System.out.println("вывод: 13.09." + year);
         }
     }
 
 
     public static boolean isLeapYear(int year) {
-        int x = year;
+        int x = year; /** Названия переменных лучше придумывать "говорящими"*/
         int y = year;
         int z = year;
+
+        /** Можно вместо цикла while использовать цикл for, тогда вообще не нужно будет объявлять и не придумывать
+        названия переменным x,y,z
+
+        for (int i = year; i >= 0; i -= 400) { // i -= 400 это сокращённое написание i = i - 400
+            if (i == 0) {
+                return true;
+            }
+        }
+        */
 
         while (x >= 0) {  // первая проверка на кратность 400
             if (x == 0) {
